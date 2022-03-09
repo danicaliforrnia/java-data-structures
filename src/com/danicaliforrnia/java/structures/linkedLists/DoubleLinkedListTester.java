b50bffc82935a30544726f47b0fd2fab9c5ed6f9
Daniel Stefanelli <d1as2n3i4e5l6>
2022-03-09 00:04:56 -0400

    feat: add Node class with double pointer (next and previous) and implement a double linked list

---
+++ src/com/danicaliforrnia/java/structures/linkedLists/DoubleLinkedListTester.java
@@ -0,0 +1,47 @@
+package com.danicaliforrnia.java.structures.linkedLists;
+
+public class DoubleLinkedListTester {
+
+    public static void main(String[] args) {
+        LinkedList<String> singlyLinkedList = new DoubleLinkedList<>();
+
+        System.out.println("--------Test 1---------");
+        singlyLinkedList.insert("Hello");
+        singlyLinkedList.insert("World");
+        singlyLinkedList.insert("World2");
+        singlyLinkedList.remove(2);
+        System.out.println("size: " + singlyLinkedList.size());
+        System.out.println(singlyLinkedList);
+
+        System.out.println("--------Test 2---------");
+        singlyLinkedList.insert("!");
+        System.out.println("size: " + singlyLinkedList.size());
+        System.out.println(singlyLinkedList);
+
+        System.out.println("--------Test 3---------");
+        singlyLinkedList.insert("¡", 0);
+        singlyLinkedList.replace("Hola", 1);
+        singlyLinkedList.replace("Mundo", 2);
+        System.out.println("size: " + singlyLinkedList.size());
+        System.out.println(singlyLinkedList);
+
+        System.out.println("--------Test 4---------");
+        singlyLinkedList.remove(1);
+        singlyLinkedList.remove(0);
+        System.out.println("size: " + singlyLinkedList.size());
+        System.out.println(singlyLinkedList);
+
+        System.out.println("--------Test 5---------");
+        singlyLinkedList.remove("Mundo");
+        singlyLinkedList.remove("!");
+        System.out.println("size: " + singlyLinkedList.size());
+        System.out.println(singlyLinkedList);
+
+        System.out.println("--------Test 6---------");
+        singlyLinkedList.insert("Hello");
+        singlyLinkedList.insert("World");
+        System.out.println("size: " + singlyLinkedList.size());
+        System.out.println(singlyLinkedList);
+    }
+
+}
