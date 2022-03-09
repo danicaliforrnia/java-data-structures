96a96d02028c6ac34e0949b8c97df0e4508ef9e9
Daniel Stefanelli <d1as2n3i4e5l6>
2022-03-08 23:11:25 -0400

    feat: create LinkedList interface with the methods a LinkedList must have

---
+++ src/com/danicaliforrnia/java/structures/linkedLists/LinkedList.java
@@ -0,0 +1,47 @@
+package com.danicaliforrnia.java.structures.linkedLists;
+
+public interface LinkedList<T> {
+    /**
+     * Insert data at the tail
+     * @param data: value to insert
+     */
+    void insert(T data);
+
+    /**
+     * Insert data at an index
+     * @param data: data to insert
+     * @param index: index where insert
+     */
+    void insert(T data, int index);
+
+    /**
+     * Replace data at index
+     * @param data: data to replace
+     * @param index: index where replace
+     */
+    void replace(T data, int index);
+
+    /**
+     * Remove first appearance of data
+     * @param data: data to remove
+     */
+    void remove(T data);
+
+    /**
+     * Remove data at index
+     * @param index: index where remove
+     */
+    void remove(int index);
+
+    /**
+     * Get size of the LinkedList
+     * @return size
+     */
+    int size();
+
+    /**
+     * Check is LinkedList is empty
+     * @return true if LinkedList is empty
+     */
+    boolean isEmpty();
+}
